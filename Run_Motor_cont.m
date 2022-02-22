706952883d0f8913db1dd3904128b453fcacf118
gbabic99 <77645450+gbabic99@users.noreply.github.com>
2022-02-21 18:03:41 -0700

    Add files via upload
    slight change

--- Run_Motor_cont.m
+++ Run_Motor_cont.m
@@ -14,12 +14,12 @@ increment = dtheta/samples;         % angle scanned per sample
 s_period = dtheta/(samples*speed);  % time between samples
 acc = speed/s_period;               % acceleration from 0 to max
 
-h.SetVelParams(0,0, acc, speed);
-h.SetRelMoveDist(0,dtheta);
-h.MoveRelative(0,1==0);
+h.SetVelParams(0,0, acc, speed);    % controls motor speed
+h.SetRelMoveDist(0,dtheta);         % sets rotational motion of motor
+h.MoveRelative(0,1==0);             % tells motor to move
 
-count = 1;
-while(count < samples + 1)
+count = 0;
+while(count < samples)
     [freq,data] = VNA_Meas(Start_Freq, Stop_Freq, Num_Points, IF_Band, IP_Addr, Cal_file);
     DATA_FILE (count,:)= data;
     disp([' Step ',num2str(count)]) 
