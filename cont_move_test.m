3400c3a0681d0e464c5398d1e452914b73e328f1
gbabic99 <77645450+gbabic99@users.noreply.github.com>
2022-02-21 19:21:54 -0700

    Add files via upload
    debugged

--- cont_move_test.m
+++ cont_move_test.m
@@ -1,16 +1,18 @@
-samples = 100;
-dtheta = 30;
-
+%% Parameters
+dtheta = 30;     
+v_max = 2;      % Maximum velocity of spin (Degrees/sec)
+samples = 30;
+s_period = dtheta/(samples*v_max);
+acc = v_max/s_period; % Acceleration from velocty minimum (0) to velocity maximum in degrees/sec^2
+%% Set Velocity and Move
+h.SetVelParams(0,0, acc, v_max);
 h.SetRelMoveDist(0,dtheta);
 h.MoveRelative(0,1==0);
-h.SetVelParams(0,3,speed); % need to figure out how varying affects stuff: middle is acceleration
-
-count = 0;
+%% Action Loop
+count = 1;
 locations = zeros(1,samples);
-while(count < samples)
-    pos = h.GetPosition_Position(0);
-    if h.GetPosition_Position(0) - pos == dtheta/samples
-        locations(count) = h.GetPosition_Position(0); % test to store locations
-        count = count + 1;
-    end   
+while(count < samples+1)
+    locations(count) = h.GetPosition_Position(0); % test to store locations
+    count = count + 1;
+    pause(s_period); 
 end
